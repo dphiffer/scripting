bea4869fb9253665070943c659d95c9336db8057
Dan Phiffer <dan@phiffer.org>
2015-09-01 18:08:17 -0400

    Update website on demand

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!empty($_GET['update'])) {
+  exec('/usr/bin/git pull origin fall-2015');
+  exit;
+}
+
 require_once 'lib/markdown/Michelf/Markdown.inc.php';
 use \Michelf\Markdown;
 
