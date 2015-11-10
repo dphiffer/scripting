b4c3e01888a287de52c62bdb01eb0b86fd0df631
Dan Phiffer <dan@phiffer.org>
2015-11-10 13:01:34 -0500

    Fix for valid accounts func

--- week12/bookmarks/setup.php
+++ week12/bookmarks/setup.php
@@ -4,13 +4,13 @@ $username = 'bookmarks';
 $password = 'kdN3l1.491';
 $dsn = 'mysql:host=127.0.0.1;dbname=bookmarks';
 $db  = new PDO($dsn, $username, $password);
-$query = $db->query("
-    SELECT *
-    FROM accounts
-");
 
 function get_valid_accounts() {
 	global $db;
+	$query = $db->query("
+	    SELECT *
+	    FROM accounts
+	");
 	$valid_accounts = array();
 	foreach ($query->fetchAll() as $account) {
 		$username = $account['username'];
