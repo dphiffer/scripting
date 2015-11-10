cc1da0ec93bb4c36523ab9dc76d4db842de18c2a
Dan Phiffer <dan@phiffer.org>
2015-11-10 12:37:38 -0500

    Bookmarks!

---
+++ week12/bookmarks/setup.php
@@ -0,0 +1,15 @@
+<?php
+
+$username = 'bookmarks';
+$password = 'kdN3l1.491';
+$dsn = 'mysql:host=127.0.0.1;dbname=bookmarks';
+$db  = new PDO($dsn, $username, $password);
+$query = $db->query("
+    SELECT *
+    FROM accounts
+");
+$valid_accounts = $query->fetchAll();
+
+print_r($valid_account);
+
+?>
