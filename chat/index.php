cbc724f3a36c8865e40d7bf637175043c63104b5
Dan Phiffer <dan@phiffer.org>
2015-11-12 14:24:19 -0500

    Added some CSS finishes

--- chat/index.php
+++ chat/index.php
@@ -3,23 +3,26 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Chat demo</title>
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<div id="msgs">
-			<?php
-			
-			$msgs = glob('msg/*.txt');
-			foreach ($msgs as $filename) {
-				$msg = file_get_contents($filename);
-				echo '<p>' . htmlentities($msg) . '</p>';
-			}
-			
-			?>
+		<div id="page">
+			<div id="msgs">
+				<?php
+				
+				$msgs = glob('msg/*.txt');
+				foreach ($msgs as $filename) {
+					$msg = file_get_contents($filename);
+					echo '<p>' . htmlentities($msg) . '</p>';
+				}
+				
+				?>
+			</div>
+			<form action="submit.php" method="post">
+				<input type="text" name="msg" placeholder="Type a message here" autocomplete="off">
+				<input type="submit" value="Send">
+			</form>
 		</div>
-		<form action="submit.php" method="post">
-			<input type="text" name="msg" placeholder="Type a message here">
-			<input type="submit" value="Send">
-		</form>
 		<script src="jquery-1.11.3.min.js"></script>
 		<script src="script.js"></script>
 	</body>
--- chat/script.js
+++ chat/script.js
@@ -1,11 +1,45 @@
 jQuery(document).ready(function($) {
+	
+	var updatingScroll = false;
+	var userScrolled = false;
+	
+	function updateMsgsScroll() {
+		updatingScroll = true;
+		if (!userScrolled) {
+			var bottom = $('#msgs')[0].scrollHeight;
+			$('#msgs')[0].scrollTop = bottom;
+		}
+	}
+	updateMsgsScroll();
+	
+	$('#msgs').scroll(function() {
+		var scrollMax = $('#msgs')[0].scrollHeight;
+		var height = $('#msgs')[0].offsetHeight;
+		console.log($('#msgs')[0].scrollTop + ', ' + (scrollMax - height));
+		if (updatingScroll) {
+			updatingScroll = false;
+		} else if ($('#msgs')[0].scrollTop > scrollMax - height) {
+			console.log('done');
+			userScrolled = false;
+		} else {
+			userScrolled = true;
+		}
+	});
+	
 	$('form').submit(function(e) {
 		e.preventDefault();
+		if ($('input[name=msg]').val() == '') {
+			// Check for empty value
+			return;
+		}
+		userScrolled = false;
 		$.ajax('submit.php', {
 			method: 'POST',
 			data: $('form').serialize(),
 			success: function(msgs) {
 				$('#msgs').html(msgs);
+				updateMsgsScroll();
+				$('input[name=msg]').val('');
 			}
 		});
 	});
@@ -15,6 +49,7 @@ jQuery(document).ready(function($) {
 			method: 'POST',
 			success: function(msgs) {
 				$('#msgs').html(msgs);
+				updateMsgsScroll();
 			}
 		});
 	}, 1000);
